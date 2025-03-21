8335442483dc67c21a48eb29d352967c2dd9db62
Daniel Ibacache <dibacach@gmail.com>
2025-03-21 17:33:53 -0300

    Actualizado portafolio con proyectos más destacables

--- portafolio.js
+++ portafolio.js
@@ -1,21 +1,40 @@
-const repoEncriptador = document.querySelector('[data-repo-encriptador]');
-const demoEncriptador = document.querySelector('[data-demo-encriptador]');
-const repoPortafolio = document.querySelector('[data-repo-portafolio]');
-const demoPortafolio = document.querySelector('[data-demo-portafolio]');
+const repoEncriptador = document.querySelector("[data-repo-encriptador]");
+const demoEncriptador = document.querySelector("[data-demo-encriptador]");
+const repoAluraflix = document.querySelector("[data-repo-aluraflix]");
+const demoAluraflix = document.querySelector("[data-demo-aluraflix]");
+const repoGifExpert = document.querySelector("[data-repo-gif-expert]");
+const demoGifExpert = document.querySelector("[data-demo-gif-expert]");
+const repoAlurageek = document.querySelector("[data-repo-alurageek]");
+const demoAlurageek = document.querySelector("[data-demo-alurageek]");
 
-repoEncriptador.addEventListener('click', () => {
-    window.open('https://github.com/dibacach/encriptador', '_blank');
+repoEncriptador.addEventListener("click", () => {
+  window.open("https://github.com/dibacach/encriptador", "_blank");
 });
 
-demoEncriptador.addEventListener('click', () => {
-    window.open('https://dibacach.github.io/encriptador', '_blank');
+demoEncriptador.addEventListener("click", () => {
+  window.open("https://dibacach.github.io/encriptador", "_blank");
 });
 
-
-repoPortafolio.addEventListener('click', () => {
-    window.open('https://github.com/dibacach/portfolio', '_blank');
+repoAluraflix.addEventListener("click", () => {
+  window.open("https://github.com/dibacach/aluraflix", "_blank");
 });
 
-demoPortafolio.addEventListener('click', () => {
-    window.open('https://dibacach.github.io/portfolio', '_blank');
+demoAluraflix.addEventListener("click", () => {
+  window.open("https://dibacach.github.io/aluraflix", "_blank");
+});
+
+repoGifExpert.addEventListener("click", () => {
+  window.open("https://github.com/dibacach/react-gif-expert", "_blank");
+});
+
+demoGifExpert.addEventListener("click", () => {
+  window.open("https://dibacach.github.io/react-gif-expert", "_blank");
+});
+
+repoAlurageek.addEventListener("click", () => {
+  window.open("https://github.com/dibacach/alura-geek", "_blank");
+});
+
+demoAlurageek.addEventListener("click", () => {
+  window.open("https://dibacach.github.io/alura-geek", "_blank");
 });
